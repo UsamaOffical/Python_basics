477c2bd54fbdcd5cc186d22e5294dba52a60ad04
Usama Jameel <160001523+UsamaOffical@users.noreply.github.com>
2025-02-23 22:19:44 +0000

    string and hello world complete

---
+++ 01_basic_fundamental_python/hello_world.py
@@ -0,0 +1,7 @@
+print("hello world")
+
+# python12 jo likha hai yaha pr jo nam daina ho dae do "python==3.12 ya jo bhi version ho vo bata do"  
+                                                                   
+# python k envirment ko bana ne k liye ye command run karte hai 'conda create - n python12 python==3.12 -y'
+
+# pip install -r requirement.txt packages ko install kare ga jo is file mai describe kare hai
---
+++ 01_basic_fundamental_python/strings_type.py
@@ -0,0 +1,14 @@
+
+name : str = "Usama jameel"
+fname : str = "Muhammad Jameel"
+age : int = 23
+education : str = "Intermadiate"
+
+details = f"""
+Name : {name}
+Father Name : {fname}
+Age : {age}
+Education : {education}
+"""
+
+print(details)
